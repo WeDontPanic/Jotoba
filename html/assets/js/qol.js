d6a8d5af1bc3629b43edb6198f06d4301956f482
Yukaru <nilsrw@gmx.de>
2021-04-17 11:55:50 +0200

    improved on-click copy feedback

--- html/assets/js/qol.js
+++ html/assets/js/qol.js
@@ -10,11 +10,13 @@ $(document).on("keypress", (event) => {
     }  
 });
 
+/*
 // Copies Kanji (right side) to clipboard on click
 $('.kanji-preview.large').on("click", (event) => {
     showMessage("success", "kanji copied to clipboard.");
     copyToClipboard(event.target.innerHTML.trim());
 });
+*/
 
 // Copies Furigana to clipboard on click
 $('.furigana-kanji-container > .furigana-preview').on("click", (event) => {
@@ -25,37 +27,47 @@ $('.furigana-kanji-container > .furigana-preview').on("click", (event) => {
 // Copies translations to clipboard on double click
 $('.furigana-kanji-container > .kanji-preview').on("dblclick", (event) => {
 	event.preventDefault();
-    showMessage("success", "translation copied to clipboard.");
-    copyTranslation(event.target.parentElement.parentElement);
+    copyTranslationAndShowMessage(event.target.parentElement.parentElement);
 });
 
 // Copies translations to clipboard on double click
 $('.inline-kana-preview').on("dblclick", (event) => {
 	event.preventDefault();
-    showMessage("success", "translation copied to clipboard.");
-    copyTranslation(event.target.parentElement);
+    copyTranslationAndShowMessage(event.target.parentElement);
 });
 
 // Used by kanji/kana copy to combine all parts, starts from the flex (parent)
-function copyTranslation(textParent) {
+function copyTranslationAndShowMessage(textParent) {
     // Find all childs that are of interest
     let fullTranslation = "";
+    let onlyKanji = true;
     textParent.childNodes.forEach((entry) => {
         if (entry.classList != undefined) {
             // Kanji
             if (entry.classList.contains("furigana-kanji-container")) {
                 entry.childNodes.forEach((subEntry) => {
                     if (subEntry.classList != undefined && subEntry.classList.contains("kanji-preview")) {
-                        fullTranslation += subEntry.innerHTML.trim();
+                        let text = subEntry.innerHTML.trim();
+                        fullTranslation += text;
+                        if (text.charCodeAt(0) < 19968)
+                            onlyKanji = false;
                     }
                 });
             }
             // Kana
             if (entry.classList.contains("inline-kana-preview")) {
-                fullTranslation += entry.innerHTML.trim();
+                let text = entry.innerHTML.trim();
+                fullTranslation += text;
+                if (text.charCodeAt(0) < 19968)
+                    onlyKanji = false;
             }
-
         } 
     });
     copyToClipboard(fullTranslation);
+	
+	if (onlyKanji) {
+		showMessage("success", "kanji copied to clipboard.");
+	} else {
+		showMessage("success", "translation copied to clipboard.");
+	}
 }
