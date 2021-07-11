461f046566b022437d54502b07db4bb5c22c5854
jojii <jojii@gmx.net>
2021-07-11 20:15:52 +0200

    translate inflection strings

--- lib/japanese/src/inflection.rs
+++ lib/japanese/src/inflection.rs
@@ -1,3 +1,4 @@
+use localization::traits::Translatable;
 use strum_macros::AsRefStr;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, AsRefStr)]
@@ -31,5 +32,31 @@ impl SentencePart {
             .as_ref()
             .map(|i| i.clone())
             .unwrap_or_default()
+    }
+}
+
+impl Translatable for Inflection {
+    fn get_id(&self) -> &'static str {
+        match self {
+            Inflection::Negative => "Negative",
+            Inflection::Polite => "Polite",
+            Inflection::Present => "Present",
+            Inflection::Past => "Past",
+            Inflection::TeForm => "TeForm",
+            Inflection::Potential => "Potential",
+            Inflection::Passive => "Passive",
+            Inflection::Causative => "Causative",
+            Inflection::CausativePassive => "CausativePassive",
+            Inflection::Imperative => "Imperative",
+            Inflection::Tai => "Tai",
+        }
+    }
+
+    fn gettext<'a>(
+        &self,
+        dict: &'a localization::TranslationDict,
+        language: Option<localization::language::Language>,
+    ) -> &'a str {
+        self.pgettext(dict, "inflection", language)
     }
 }
