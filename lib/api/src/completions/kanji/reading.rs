85cbac0b598c261f4c4364616e0117d3ca96a9f7
jojii <jojii@gmx.net>
2021-08-21 20:22:22 +0200

    reimplement kanji reading search

--- lib/api/src/completions/kanji/reading.rs
+++ lib/api/src/completions/kanji/reading.rs
@@ -3,38 +3,40 @@ use std::cmp::Ordering;
 use error::api_error::RestError;
 use resources::models::kanji;
 
-use crate::completions::{Response, WordPair};
-
-/// Reading response (onyomi, kunyomi)
-pub(crate) struct ReadingsRes(Vec<String>, Vec<String>);
+use crate::completions::{Response, SuggestionType, WordPair};
 
 /// Gets suggestions for kanji reading search eg: "痛 いた.い"
 pub async fn suggestions(kanji_reading: kanji::Reading) -> Result<Response, RestError> {
-    /*
+    let kanji_storage = resources::get().kanji();
+
     let literal = kanji_reading.literal;
     let reading = kanji_reading.reading.replace("。", "").replace(".", "");
 
-    // Find readings
-    let readings = readings_by_lit(client, literal)
-        .await?
-        .map(|i| i.get_suggestions(literal));
+    let character = kanji_storage
+        .by_literal(literal)
+        .ok_or(RestError::NotFound)?;
 
-    if readings.is_none() {
+    let mut readings = character
+        .kunyomi
+        .iter()
+        .chain(character.onyomi.iter())
+        .flatten()
+        .map(|i| WordPair {
+            primary: i.clone(),
+            secondary: Some(literal.to_string()),
+        })
+        .collect::<Vec<_>>();
+
+    if readings.is_empty() {
         return Ok(Response::default());
     }
-    let mut readings = readings.unwrap();
 
-    // User provided additionally a part of a reading
-    if !reading.is_empty() {
-        readings.sort_by(|a, b| order(a, b, &reading));
-    }
+    readings.sort_by(|a, b| order(a, b, &reading));
 
     Ok(Response {
         suggestions: readings,
         suggestion_type: SuggestionType::KanjiReading,
     })
-    */
-    Err(RestError::Internal)
 }
 
 fn order(a: &WordPair, b: &WordPair, reading: &str) -> Ordering {
@@ -42,41 +44,5 @@ fn order(a: &WordPair, b: &WordPair, reading: &str) -> Ordering {
 }
 
 fn starts_with(word: &WordPair, reading: &str) -> bool {
-    word.primary.replace(".", "").starts_with(&reading)
-}
-
-impl ReadingsRes {
-    /// Returns all suggestions of the [`KanjiRes`]
-    fn get_suggestions(self, literal: char) -> Vec<WordPair> {
-        Self::to_word_pair(self.0, literal)
-            .chain(Self::to_word_pair(self.1, literal))
-            .collect()
-    }
-
-    /// Returns an iterator over word paris with the passed readings
-    fn to_word_pair(readings: Vec<String>, literal: char) -> impl Iterator<Item = WordPair> {
-        readings.into_iter().map(move |i| WordPair {
-            primary: i,
-            secondary: Some(literal.to_string()),
-        })
-    }
-}
-
-/// Returns a single item of [`ReadingsRes`] for the kanji identified by its literal
-async fn readings_by_lit(literal: char) -> Result<Option<ReadingsRes>, RestError> {
-    /*
-    let client = client.get().await?;
-
-    let query = "SELECT onyomi, kunyomi FROM kanji WHERE literal = $1 LIMIT 1";
-
-    let statement = client.prepare_cached(query).await?;
-
-    let res = client
-        .query_opt(&statement, &[&literal.to_string()])
-        .await?
-        .map(|i| ReadingsRes::from(i));
-
-    Ok(res)
-    */
-    Ok(None)
+    word.primary.replace(".", "").starts_with(reading)
 }
