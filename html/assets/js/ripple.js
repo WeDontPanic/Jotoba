9ea98eddfc2d650c67c8dbfbb23f8e05aed0025c
Yukaru <nilsrw@gmx.de>
2021-08-25 02:27:34 +0200

    change paginating path && implement ripple effect

---
+++ html/assets/js/ripple.js
@@ -0,0 +1,23 @@
+function createRipple(e)
+{
+  if(this.getElementsByClassName('rippleAnim').length > 0)
+    {
+      this.removeChild(this.childNodes[1]);
+    }
+  
+  var circle = document.createElement('div');
+  this.appendChild(circle);
+  
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + 'px';
+  
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
+  circle.style.top = e.pageY - this.offsetTop - d / 2 + 'px';
+  
+  circle.classList.add('rippleAnim');
+}
+
+document.querySelectorAll(".ripple").forEach((e) => {
+	console.log(e);
+  e.addEventListener('click', createRipple);
+});
