dfb30cfec3d50da49704343a4da9fa4d2f818d9c
jojii <jojii@gmx.net>
2021-09-17 18:53:03 +0200

    improve suggestion file format for names

--- lib/api/src/completions/storage.rs
+++ lib/api/src/completions/storage.rs
@@ -1,9 +1,4 @@
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-    path::{Path, PathBuf},
-};
+use std::{error::Error, fs::File, path::Path};
 
 use config::Config;
 use log::info;
@@ -24,6 +19,7 @@ pub(crate) static NAME_TRANSCRIPTIONS: OnceCell<TextSearch<Vec<NameTranscription
 pub(crate) static K_MEANING_SUGGESTIONS: OnceCell<TextSearch<Vec<KanjiMeaningSuggestionItem>>> =
     OnceCell::new();
 
+/// Load all available suggestions
 pub fn load_suggestions(config: &Config) -> Result<(), Box<dyn Error>> {
     load_meaning_suggestions(&config)?;
     load_native_names(&config)?;
@@ -32,7 +28,7 @@ pub fn load_suggestions(config: &Config) -> Result<(), Box<dyn Error>> {
 }
 
 /// A single suggestion item for kanji meanings
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Deserialize)]
 pub struct KanjiMeaningSuggestionItem {
     pub meaning: String,
     pub literal: char,
@@ -41,18 +37,18 @@ pub struct KanjiMeaningSuggestionItem {
     pub score: i32,
 }
 
-#[derive(Clone, Debug)]
+/// A suggestion Item for transcribed a name
+#[derive(Deserialize)]
 pub struct NameTranscription {
     pub name: String,
+    #[serde(deserialize_with = "eudex_deser")]
     pub hash: eudex::Hash,
 }
 
-fn eudex_deser<'de, D>(deserializer: D) -> Result<eudex::Hash, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s: u64 = Deserialize::deserialize(deserializer)?;
-    Ok(eudex::Hash::from(s))
+/// A suggestion Item for a name item in japanese
+#[derive(Deserialize)]
+pub struct NameNative {
+    pub name: String,
 }
 
 impl store_item::Item for KanjiMeaningSuggestionItem {
@@ -82,26 +78,6 @@ impl Into<WordPair> for &KanjiMeaningSuggestionItem {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum SuggestionVersion {
-    V0,
-    V1,
-}
-
-trait Parseable: Sized {
-    fn parse(s: &str, version: SuggestionVersion) -> Result<Self, error::Error>;
-}
-
-impl Parseable for NameTranscription {
-    #[inline]
-    fn parse(s: &str, _version: SuggestionVersion) -> Result<Self, error::Error> {
-        Ok(NameTranscription {
-            name: s.to_owned(),
-            hash: eudex::Hash::new(s),
-        })
-    }
-}
-
 impl store_item::Item for NameTranscription {
     #[inline]
     fn get_text(&self) -> &str {
@@ -124,18 +100,6 @@ impl Into<WordPair> for &NameTranscription {
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct NameNative {
-    pub name: String,
-}
-
-impl Parseable for NameNative {
-    #[inline]
-    fn parse(s: &str, _version: SuggestionVersion) -> Result<Self, error::Error> {
-        Ok(NameNative { name: s.to_owned() })
-    }
-}
-
 impl store_item::Item for NameNative {
     #[inline]
     fn get_text(&self) -> &str {
@@ -152,6 +116,7 @@ impl Into<WordPair> for &NameNative {
         }
     }
 }
+
 /// Load kanji meaning suggestion file into memory
 fn load_meaning_suggestions(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
     let file = Path::new(config.get_suggestion_sources()).join("kanji_meanings");
@@ -178,7 +143,7 @@ fn load_native_names(config: &Config) -> Result<(), Box<dyn std::error::Error>> 
         return Ok(());
     }
 
-    let items: Vec<NameNative> = load_file(&file)?;
+    let items: Vec<NameNative> = bincode::deserialize_from(File::open(file)?)?;
 
     NAME_NATIVE.set(TextSearch::new(items)).ok();
 
@@ -195,7 +160,7 @@ fn load_name_transcriptions(config: &Config) -> Result<(), Box<dyn std::error::E
         return Ok(());
     }
 
-    let items: Vec<NameTranscription> = load_file(&file)?;
+    let items: Vec<NameTranscription> = bincode::deserialize_from(File::open(file)?)?;
 
     NAME_TRANSCRIPTIONS.set(TextSearch::new(items)).ok();
 
@@ -204,23 +169,11 @@ fn load_name_transcriptions(config: &Config) -> Result<(), Box<dyn std::error::E
     Ok(())
 }
 
-/// Parse a single suggestion file
-fn load_file<T: Parseable>(path: &PathBuf) -> Result<Vec<T>, error::Error> {
-    let file = File::open(path)?;
-
-    let mut lines = BufReader::new(file).lines();
-
-    let first = lines.next().ok_or(error::Error::ParseError)??;
-
-    let version = match first.as_str() {
-        "v1" => SuggestionVersion::V1,
-        _ => SuggestionVersion::V0,
-    };
-
-    lines
-        .map(|i| {
-            i.map_err(|i| i.into())
-                .and_then(move |i| T::parse(&i, version))
-        })
-        .collect::<Result<_, _>>()
+#[inline]
+fn eudex_deser<'de, D>(deserializer: D) -> Result<eudex::Hash, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: u64 = Deserialize::deserialize(deserializer)?;
+    Ok(eudex::Hash::from(s))
 }
