901f776059b027349405ca3999e9661d861393d6
Yukaru <nilsrw@gmx.de>
2021-09-23 13:53:11 +0200

    finish image cropping frontend-side

--- html/assets/js/imageSearch.js
+++ html/assets/js/imageSearch.js
@@ -2,6 +2,16 @@
  *  This file handles everything related to image-search requests
  */ 
 
+// Prepare to correctly close the Modal when clicking on the darkened parts
+Util.awaitDocumentReady(() => {
+    var target = $("#imageCroppingModal")[0];
+    $(target).on("click", (e) => {
+        if (e.target == target) {
+            resetUploadUrlInput();
+        }
+    });
+});
+
 // Shows / Hides the image search overlay
 function toggleImageSearchOverlay() {
     let overlay = $('.overlay.image');
@@ -24,7 +34,11 @@ function imgUploadAltClick() {
 // Blocks the URL input upon file selection
 function imgSearchFileSelected() {
     let fileInput = document.getElementById("imgUploadFile").files[0];
-    disableUploadUrlInput(fileInput.name);
+    if (fileInput !== undefined) {
+        disableUploadUrlInput(fileInput.name);
+    } else {
+        resetUploadUrlInput();
+    }
 }
 
 // Toggles the URL input active / disabled
@@ -36,8 +50,14 @@ function resetUploadUrlInput() {
     urlInput.classList.remove("disabled");
     urlInput.disabled = false;
     urlInputDisabled = false;
-
     urlInput.placeholder = originalMsg; 
+
+    document.getElementById("imgUploadFile").value = null;
+
+    if (croppedImage !== null) {
+        document.getElementById("croppingTarget").innerHTML = "";
+        croppedImage = null;
+    }
 }
 
 function disableUploadUrlInput(newMessage) {
@@ -51,22 +71,31 @@ function disableUploadUrlInput(newMessage) {
 }
 
 // Opens the Image Cropping Overlay
+var croppedImage = null;
 function openImageCropOverlay() {
-    // ImageCropper(parent, targetFile, settings) -> targetFile kann direkt eine URL aus dem Browser sein!
-    // new ImageCropper(".image-upload-parent", todo,{max_width:300,max_height:300})
-
     var selectedFiles = document.getElementById("imgUploadFile").files;
+    var inputUrl = document.getElementById("imgUploadUrl").value;
 
     if (selectedFiles.length > 0) {
-        var reader  = new FileReader();
+        let reader = new FileReader();
         reader.onload = function(e) {
-            new ImageCropper("#croppingTarget", e.target.result, {max_width:300,max_height:300}) 
+            croppedImage = new ImageCropper("#croppingTarget", e.target.result, {max_width:300,max_height:300}) 
         }
         reader.readAsDataURL(selectedFiles[0]);
+        $("#imageCroppingModal").modal();
     }
-    else {
-        new ImageCropper("#croppingTarget", document.getElementById("imgUploadUrl").value, {max_width:300,max_height:300}) 
+    else if (inputUrl.length > 0 && /\.(jpg|jpeg|png)$/.test(inputUrl)) {  
+        croppedImage = new ImageCropper("#croppingTarget", document.getElementById("imgUploadUrl").value, {max_width:300,max_height:300}) 
+        $("#imageCroppingModal").modal();
+    } else {
+        Util.showMessage("error", "You need to enter a URL or upload a file!");
+    }
+}
+
+function uploadCroppedImage() {
+    if (croppedImage !== null) {
+        console.log(croppedImage.crop("image/png", 1));
     }
 
-    // Modal + #croppingTarget => TODO
+    resetUploadUrlInput();
 }
