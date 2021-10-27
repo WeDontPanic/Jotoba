69225abc6d4c0f2ca80c175a1efd21ce36e17f71
jojii <jojii@gmx.net>
2021-10-27 17:23:06 +0200

    finish rad picker backend

--- lib/api/src/radical/search/jp_search.rs
+++ lib/api/src/radical/search/jp_search.rs
@@ -1,4 +1,7 @@
+use error::Error;
 use itertools::Itertools;
+use japanese::JapaneseExt;
+use search::engine::{self, SearchTask};
 
 /// Returns a list of radicals based on the radical-search `query`
 pub fn search(query: &str) -> Vec<char> {
@@ -6,8 +9,7 @@ pub fn search(query: &str) -> Vec<char> {
         return kanji_search(query);
     }
 
-    // TODO:
-    vec![]
+    kana_search(query).unwrap_or_default()
 }
 
 /// Takes all kanji from `query` and returns a list of all unique radicals to build all kanji
@@ -21,5 +23,38 @@ fn kanji_search(query: &str) -> Vec<char> {
         .flatten()
         .copied()
         .unique()
-        .collect::<Vec<char>>()
+        .collect()
+}
+
+/// Does a kana word-search and returns some likely radicals for the given query
+fn kana_search(query: &str) -> Result<Vec<char>, Error> {
+    let mut search_task: SearchTask<engine::words::native::Engine> =
+        SearchTask::new(&query).limit(3).threshold(0.8f32);
+
+    let original_query = query.to_string();
+    search_task.set_order_fn(move |word, rel, q_str, _| {
+        search::word::order::japanese_search_order(word, rel, q_str, Some(&original_query))
+    });
+
+    let kanji_retr = resources::get().kanji();
+    let res = search_task
+        .find()?
+        .item_iter()
+        .map(|i| {
+            let reading = &i.get_reading().reading;
+            reading
+                .chars()
+                .filter(|i| i.is_kanji())
+                .collect::<Vec<char>>()
+        })
+        .flatten()
+        .unique()
+        .filter_map(|kanji| kanji_retr.by_literal(kanji).and_then(|i| i.parts.as_ref()))
+        .flatten()
+        .unique()
+        .copied()
+        .take(10)
+        .collect::<Vec<_>>();
+
+    Ok(res)
 }
