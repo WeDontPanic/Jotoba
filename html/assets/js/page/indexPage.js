74fff455ed0b9eabbfc553951cfc6ee6bbaaa405
Yukaru <nilsrw@gmx.de>
2021-11-09 17:57:37 +0100

    add green-dot on help-icon for first-time visitors

--- html/assets/js/page/indexPage.js
+++ html/assets/js/page/indexPage.js
@@ -7,6 +7,18 @@ if (storedVersion < jotobaVersion) {
     document.getElementsByClassName("notificationBtn")[0].classList.add("update");
 }
 
+/* Check if user is the first time on Jotoba */
+if (localStorage.length == 0) {
+    document.getElementsByClassName("infoBtn")[0].classList.add("new");
+}
+
+// Opens the Patch Notes of the latest release
+function openHelpPage() {
+    document.getElementsByClassName("infoBtn")[0].classList.remove("new");
+    localStorage.setItem("first_time", "false");
+    Util.loadUrl("/help");
+}
+
 // Opens the Patch Notes of the latest release
 function openNotifications() {
     localStorage.setItem("joto_version", jotobaVersion);
