da62406390b80d984a78bc637626861ab9c976bf
clnhs <git@colinhs.com>
2021-11-18 23:51:23 -0500

    Back to original formatting.

--- html/assets/js/tools/theme.js
+++ html/assets/js/tools/theme.js
@@ -1,7 +1,7 @@
 // Sets the color Theme to the given Value by passing a class to the :root element
 const setTheme = (theme) => {
-  document.documentElement.className = theme;
-  localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
 }
 
 // On load -> Set the Color Theme
@@ -9,8 +9,8 @@ window.matchMedia("(prefers-color-scheme: dark)").matches ? setTheme("dark") : s
 
 // On load -> listen for prefers-color-scheme change
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener(
-  "change",
-  e => setTheme(e.matches ? "dark" : "light")
+    "change",
+    e => setTheme(e.matches ? "dark" : "light")
 );
 
 // On load -> Check if there is a theme stored already
@@ -19,7 +19,7 @@ theme && setTheme(theme);
 
 // Updates theme when changed by another tab (or console)
 const themeUpdater = window.setInterval(() => {
-  theme = localStorage.getItem('theme');
-  if (theme !== null)
-    setTheme(theme);
+    theme = localStorage.getItem('theme');
+    if (theme !== null)
+        setTheme(theme);
 }, 500);
