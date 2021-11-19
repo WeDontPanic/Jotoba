b0fae65efaa9d5408af3db59901bc28a07fa61fe
clnhs <git@colinhs.com>
2021-11-19 10:53:38 -0500

    Removed Util.hexToRgb per request.

--- html/assets/js/tools/utils.js
+++ html/assets/js/tools/utils.js
@@ -38,13 +38,3 @@ Util.loadScript = function(url, async, attributes, callback) {
     // Append and load
     document.head.appendChild(s);
 }
-
-// Converts a hex value to rgb
-Util.hexToRgb = function(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
