a9c226c40d1a5ac68a11da89d02303dbfb44a975
Yukaru <nilsrw@gmx.de>
2021-11-25 21:33:10 +0100

    fix info btn not working anywhere but index

--- html/assets/js/page/indexPage.js
+++
@@ -1,7 +0,0 @@
-// Opens the Help Page
-function openHelpPage() {
-    document.getElementsByClassName("infoBtn")[0].classList.remove("new");
-    if (localStorage != null)
-        localStorage.setItem("first_time", "false");
-    Util.loadUrl("/help");
-}
--- html/assets/js/search/search.js
+++ html/assets/js/search/search.js
@@ -217,3 +217,11 @@ function closeAllSubSearchbarOverlays(overlayToIgnore) {
     if (overlayToIgnore !== "image")
         $('.overlay.image').addClass('hidden');
 }
+
+// Opens the Help Page
+function openHelpPage() {
+    document.getElementsByClassName("infoBtn")[0].classList.remove("new");
+    if (localStorage != null)
+        localStorage.setItem("first_time", "false");
+    Util.loadUrl("/help");
+}
