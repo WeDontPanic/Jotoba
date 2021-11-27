0eff380e60985ebc9c51202c92a226fe1b194d0a
jojii <jojii@gmx.net>
2021-11-27 14:13:17 +0100

    update dependencies

--- lib/frontend/src/actix_ructe.rs
+++ lib/frontend/src/actix_ructe.rs
@@ -14,7 +14,7 @@ macro_rules! render {
 
 pub struct Render<T: FnOnce(&mut dyn Write) -> std::io::Result<()>>(pub T);
 
-impl<T: FnOnce(&mut dyn Write) -> std::io::Result<()>> From<Render<T>> for actix_web::body::Body {
+impl<T: FnOnce(&mut dyn Write) -> std::io::Result<()>> From<Render<T>> for actix_web::body::AnyBody {
     fn from(t: Render<T>) -> Self {
         let mut buf = Vec::new();
         match t.0(&mut buf) {
