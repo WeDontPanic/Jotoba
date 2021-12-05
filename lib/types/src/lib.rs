923ea0d761d9166d1315d7cbe855be950ee6d449
jojii <jojii@gmx.net>
2021-12-05 21:13:43 +0100

    add basic API wrapper filestructure

--- lib/types/src/lib.rs
+++ lib/types/src/lib.rs
@@ -1,6 +1,11 @@
-#[cfg(feature = "raw_types")]
 /// Contains raw data structures used for parsing and generating the 'real' resources
+#[cfg(feature = "raw_types")]
 pub mod raw;
 
 /// Contains all information holding structures for jotoba resources
+#[cfg(feature = "jotoba")]
 pub mod jotoba;
+
+/// Contains all structures and informations required for the API
+#[cfg(feature = "api")]
+pub mod api;
