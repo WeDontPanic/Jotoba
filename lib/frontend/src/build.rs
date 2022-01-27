4fbf112a4254c905b44daead3c722380ba3a0bcc
Yukaru <nilsrw@gmx.de>
2022-01-27 17:59:56 +0100

    simplify build.rs

--- lib/frontend/src/build.rs
+++ lib/frontend/src/build.rs
@@ -2,9 +2,5 @@ use ructe::{Result, Ructe};
 
 fn main() -> Result<()> {
     let mut ructe = Ructe::from_env()?;
-    ructe.compile_templates("templates")?;
-    ructe.compile_templates("templates/overlays")?;
-    ructe.compile_templates("templates/overlays/page")?;
-    ructe.compile_templates("templates/overlays/searchbar")?;
-    ructe.compile_templates("templates/subtemplates")
+    ructe.compile_templates("templates")
 }
