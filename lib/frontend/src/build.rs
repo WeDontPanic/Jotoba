a9755f1f3f34c8dda2c411ab77de3e3edbf87b95
Yukaru <nilsrw@gmx.de>
2022-01-27 17:55:53 +0100

    reorganize templates

--- lib/frontend/src/build.rs
+++ lib/frontend/src/build.rs
@@ -3,5 +3,8 @@ use ructe::{Result, Ructe};
 fn main() -> Result<()> {
     let mut ructe = Ructe::from_env()?;
     ructe.compile_templates("templates")?;
+    ructe.compile_templates("templates/overlays")?;
+    ructe.compile_templates("templates/overlays/page")?;
+    ructe.compile_templates("templates/overlays/searchbar")?;
     ructe.compile_templates("templates/subtemplates")
 }
