680a2f22157df8883b9c42b436d2f9a88d395a17
jojii <jojii@gmx.net>
2022-07-01 19:08:15 +0200

    add tags and fix docker image

--- html/assets/js/search/shared.js
+++ html/assets/js/search/shared.js
@@ -4,12 +4,12 @@
 
 const kanjiRegEx = '([一-龯|々|𥝱|𩺊])';
 const hashtags = [
-    "#adverb", "#auxilary", "#conjungation", "#noun", "#prefix", "#suffix", "#particle", "#sfx",
-    "#verb", "#adjective", "#counter", "#expression", "#interjection", "#pronoun", "#numeric", "#transitive", "#intransitive",
-    "#unclassified", "#word", "#sentence", "#name", "#kanji", "#abbreviation", "#N5", "#N4", "#N3", "#N2", "#N1", "#hidden", "#Irregular-Ichidan"
+  "#adverb", "#auxilary", "#conjungation", "#noun", "#prefix", "#suffix", "#particle", "#sfx",
+  "#verb", "#adjective", "#counter", "#expression", "#interjection", "#pronoun", "#numeric", "#transitive", "#intransitive",
+  "#unclassified", "#word", "#sentence", "#name", "#kanji", "#abbreviation", "#N5", "#N4", "#N3", "#N2", "#N1", "#JLPT5", "#JLPT4", "#JLPT3", "#JLPT2", "#JLPT1", "#hidden", "#Irregular-Ichidan"
 ];
 
-var currentSuggestion = ""; 
+var currentSuggestion = "";
 var currentSuggestionIndex = 0; // 0 => nothing
 var availableSuggestions = 0;
 var preventNextApiCall = false;
@@ -17,9 +17,9 @@ var preventNextApiCall = false;
 var input, searchRow, shadowText, sContainer, rContainer;
 
 Util.awaitDocumentInteractive(() => {
-    input = document.getElementById("search");
-    searchRow = document.getElementById("search-row");
-    shadowText = document.getElementById("shadow-text");
-    sContainer = document.getElementById("suggestion-container");
-    rContainer = document.getElementById("suggestion-container-rad");
+  input = document.getElementById("search");
+  searchRow = document.getElementById("search-row");
+  shadowText = document.getElementById("shadow-text");
+  sContainer = document.getElementById("suggestion-container");
+  rContainer = document.getElementById("suggestion-container-rad");
 });
