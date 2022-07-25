21d8009e90643afd2e45635679f7a2983259db50
jojii <jojii@gmx.net>
2022-07-25 12:34:11 +0200

    sort possible rads by stroke count

--- lib/api/src/radical/kanji.rs
+++ lib/api/src/radical/kanji.rs
@@ -13,7 +13,7 @@ pub async fn kanji_by_radicals(payload: Json<Request>) -> Result<Json<Response>,
 }
 
 pub fn find_kanji(rads: &[char]) -> Response {
-    let mut possible_radicals = IntSet::with_capacity(rads.len() * 3);
+    let mut possible_rads_set = IntSet::with_capacity(rads.len() * 3);
     let mut kanji_res: IntMap<Vec<char>> = IntMap::with_capacity(8);
 
     let k_retrieve = resources::get().kanji();
@@ -21,25 +21,31 @@ pub fn find_kanji(rads: &[char]) -> Response {
         push_or_insert(&mut kanji_res, kanji.stroke_count as u32, kanji.literal);
 
         if !kanji.parts.is_empty() {
-            possible_radicals.reserve(kanji.parts.len());
-            possible_radicals.extend(kanji.parts.iter().map(|i| *i as u32));
+            possible_rads_set.reserve(kanji.parts.len());
+            possible_rads_set.extend(kanji.parts.iter().map(|i| *i as u32));
         }
     }
 
-    let mut possible_radicals2 = Vec::with_capacity(possible_radicals.len());
-    possible_radicals2.extend(
-        possible_radicals
+    let mut possible_rads = Vec::with_capacity(possible_rads_set.len());
+    possible_rads.extend(
+        possible_rads_set
             .iter()
             .map(|i| unsafe { char::from_u32_unchecked(i) }),
     );
+    sort_by_stroke_order(&mut possible_rads);
 
-    let mut kanji_res2: HashMap<u32, Vec<char>> = HashMap::with_capacity(kanji_res.len());
+    let mut kanji_res2 = HashMap::<u32, Vec<char>>::with_capacity(kanji_res.len());
     kanji_res2.extend(kanji_res);
 
     Response {
-        possible_radicals: possible_radicals2,
+        possible_radicals: possible_rads,
         kanji: kanji_res2,
     }
+}
+
+#[inline]
+fn sort_by_stroke_order(inp: &mut [char]) {
+    inp.sort_by_cached_key(|i| japanese::radicals::get_radical(*i).unwrap().1);
 }
 
 fn push_or_insert<T>(map: &mut IntMap<Vec<T>>, key: u32, item: T) {
